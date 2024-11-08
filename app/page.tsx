c9e58b2d64b65932620d0a8218469e30614355ed
Ricardo Garcia Bahena <garci283@cougars.csusm.edu>
2024-11-07 18:29:28 -0800

    fix issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+
 
 
 export default function Component() {
